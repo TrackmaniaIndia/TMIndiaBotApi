d83fc11dd640356bfa895b2d7493e4a8fa561a0a
Ashwin Sharma <anupshrma2007@gmail.com>
2021-12-14 16:17:12 +0530

    add route `/tm2020/totd/latest`

---
+++ Routes/tm2020/totdToday.js
@@ -0,0 +1,22 @@
+const { client } = require("../../tmio.js");
+const cache = require('memory-cache');
+const cb = require('../../cacheTimeoutCb.js')
+
+module.exports.handle = (app) => {
+    app.get('/tm2020/totd/latest', async (req, res) => {
+        const cacheEntry = cache.get(`tm2020:totd`)
+        if(cacheEntry !== null) {
+            const data = JSON.parse(cacheEntry);
+            return res.send(data);
+        }
+
+        const totd = await client.totd.get(new Date())
+        let map = await totd.map();
+        map = map._data
+
+        cache.put(`tm2020:totd`, JSON.stringify(data), 3600000, cb) // 1 hour
+        res.send(map)
+    })
+};
+
+module.exports.registerdRoutes = ["/tm2020/totd/latest"];
