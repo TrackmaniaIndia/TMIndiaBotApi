d87b6184ed419a91045ae51ec516b084871dc4c9
Deepesh Nimma <deepnimma@gmail.com>
2021-12-14 20:55:22 +0530

    cotd endpoint uses id instead of username

--- Routes/tm2020/playerCotd.js
+++ Routes/tm2020/playerCotd.js
@@ -3,35 +3,16 @@ const cache = require('memory-cache');
 const cb = require('../../cacheTimeoutCb.js')
 
 module.exports.handle = (app) => {
-    app.get("/tm2020/player/:username/cotd/", async (req, res) => {
-        const accUsername = req.params.username;
-        // const page = req.params.page;
+    app.get("/tm2020/player/:id/cotd/", async (req, res) => {
+        const accId = req.params.id;
 
-        const cacheEntry = cache.get(`tm2020:player:${accUsername}:cotd`)
+        const cacheEntry = cache.get(`tm2020:player:${accId}:cotd`)
         if (cacheEntry !== null) {
             const data = JSON.parse(cacheEntry);
             return res.send(data);
         }
 
-        // if (page !== undefined && isNaN(page)) {
-        //     res.status(400);
-        //     res.json({
-        //         error: "INVALID_PAGE",
-        //         msg: "An invalid page number was provided",
-        //     });
-        // }
-
-        const searchResults = await client.players.search(accUsername);
-        if (searchResults[0] === undefined) {
-            res.status(400);
-            res.json({
-                error: "INVALID_USERNAME",
-                msg: "An invalid username was provided",
-            });
-            return;
-        }
-
-        const player_data = await client.players.get(searchResults[0].id);
+        const player_data = await client.players.get(accId);
         page = 0
         let next_page = await player_data.cotd(page);
         let player_cotd_data = next_page
@@ -50,9 +31,9 @@ module.exports.handle = (app) => {
 
         const cotdData = player_cotd_data._data
 
-        cache.put(`tm2020:player:${accUsername}:cotd`, JSON.stringify(cotdData), 7200000, cb) // 2 hours
+        cache.put(`tm2020:player:${accId}:cotd`, JSON.stringify(cotdData), 7200000, cb) // 2 hours
         res.send(cotdData);
     });
 };
 
-module.exports.registerdRoutes = ["/tm2020/player/:id/cotd/:page"];
+module.exports.registerdRoutes = ["/tm2020/player/:id/cotd/"];
