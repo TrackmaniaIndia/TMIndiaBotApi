c2676897241eb518076a041beedf097d6217b548
Deepesh Nimma <deepnimma@gmail.com>
2021-12-19 19:14:00 +0530

    Add Map Leaderboards

---
+++ Routes/tm2020/maplbs.js
@@ -0,0 +1,36 @@
+const { client } = require("../../tmio.js");
+
+// DO NOT CACHE THIS
+module.exports.handle = (app) => {
+    app.get("/tm2020/leaderboard/:map_id", async (req, res) => {
+        const map_id = req.params.map_id;
+
+        console.log('Got request')
+        map_data = await client.maps.get(map_id);
+        more_leaderboard = await map_data.leaderboardLoadMore();
+
+        console.log("Requests Remaining: " + client.ratelimit.remaining)
+
+        let player_leaderboards = new Array
+
+        for (let i = 0; i < map_data.leaderboard.length; i++) {
+            player_leaderboards.push(map_data.leaderboard[i]._data);
+        }
+
+        for (let i = 0; i < more_leaderboard.length; i++) {
+            try {
+                let name = more_leaderboard[i]._data['player']['name']
+                player_leaderboards.push(more_leaderboard[i]._data);
+            }
+            catch {
+                continue;
+            }
+            // if(more_leaderboard._data != null)
+
+        }
+
+        res.send(player_leaderboards)
+    })
+};
+
+module.exports.registerdRoutes = ["/tm2020/leaderboard/:map_id"];
