5a47ef98f198279ce939a126514a5e6b8fe3b8b1
Deepesh Nimma <deepnimma@gmail.com>
2021-12-21 17:41:59 +0530

    catch all for when tmx is down

--- Routes/tm2020/totdToday.js
+++ Routes/tm2020/totdToday.js
@@ -5,21 +5,25 @@ const cb = require('../../cacheTimeoutCb.js')
 
 module.exports.handle = (app) => {
     app.get('/tm2020/totd/latest', async (req, res) => {
-        const cacheEntry = cache.get(`tm2020:totd`)
-        if (cacheEntry !== null) {
-            const data = JSON.parse(cacheEntry);
-            return res.send(data);
+        try {
+            const cacheEntry = cache.get(`tm2020:totd`)
+            if (cacheEntry !== null) {
+                const data = JSON.parse(cacheEntry);
+                return res.send(data);
+            }
+
+            const totd = await client.totd.get(new Date())
+            let map = await totd.map();
+            map = map._data
+            map.name = client.formatTMText(map.name);
+
+            map.name = client.formatTMText(map.name)
+
+            cache.put(`tm2020:totd`, JSON.stringify(map), 3600000, cb) // 1 hour
+            res.send(map)
+        } catch (err) {
+            res.send({ 'error': 'tmx is down' })
         }
-
-        const totd = await client.totd.get(new Date())
-        let map = await totd.map();
-        map = map._data
-        map.name = client.formatTMText(map.name);
-
-        map.name = client.formatTMText(map.name)
-
-        cache.put(`tm2020:totd`, JSON.stringify(map), 3600000, cb) // 1 hour
-        res.send(map)
     })
 };
 
